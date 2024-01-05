04ba2842f9bf243dc499263be3fd4b00693cef0e
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-05 15:52:12 -0500

    fix? type issue

--- src/app/studio/[[...index]]/studio.tsx
+++ src/app/studio/[[...index]]/studio.tsx
@@ -1,8 +1,8 @@
 'use client';
-
 import { NextStudio } from 'next-sanity/studio';
+import type { Config as ConfigNative } from 'sanity';
 import config from '../../../../sanity.config';
 
 export function Studio() {
-  return <NextStudio config={config} />;
+  return <NextStudio config={config as ConfigNative} />;
 }
