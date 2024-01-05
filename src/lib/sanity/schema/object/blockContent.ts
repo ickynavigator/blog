fc614eaaa5e1ea1cb8f9bd1ab4444be6eafd2b82
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-05 18:01:32 -0500

    updated block content

--- src/lib/sanity/schema/object/blockContent.ts
+++ src/lib/sanity/schema/object/blockContent.ts
@@ -1,4 +1,8 @@
-import { defineArrayMember, defineType } from '@sanity-typed/types';
+import {
+  defineArrayMember,
+  defineField,
+  defineType,
+} from '@sanity-typed/types';
 
 export default defineType({
   title: 'Block Content',
@@ -9,26 +13,47 @@ export default defineType({
       title: 'Block',
       type: 'block',
       styles: [
-        { title: 'Normal', value: 'normal' },
-        { title: 'H1', value: 'h1' },
-        { title: 'H2', value: 'h2' },
-        { title: 'H3', value: 'h3' },
-        { title: 'H4', value: 'h4' },
-        { title: 'Quote', value: 'blockquote' },
-      ] as const,
-      lists: [{ title: 'Bullet', value: 'bullet' }] as const,
+        { title: 'Normal', value: 'normal' as const },
+        { title: 'H1', value: 'h1' as const },
+        { title: 'H2', value: 'h2' as const },
+        { title: 'H3', value: 'h3' as const },
+        { title: 'H4', value: 'h4' as const },
+        { title: 'Quote', value: 'blockquote' as const },
+      ],
+      lists: [
+        { title: 'Bullet', value: 'bullet' as const },
+        { title: 'Bar', value: 'bar' as const },
+      ],
       marks: {
         decorators: [
-          { title: 'Strong', value: 'strong' },
-          { title: 'Emphasis', value: 'em' },
-        ] as const,
+          { title: 'Strong', value: 'strong' as const },
+          { title: 'Emphasis', value: 'em' as const },
+        ],
         annotations: [
-          {
-            title: 'URL',
+          defineArrayMember({
             name: 'link',
             type: 'object',
-            fields: [{ title: 'URL', name: 'href', type: 'url' }],
-          },
+            title: 'Link',
+            fields: [
+              defineField({
+                name: 'href',
+                type: 'url',
+                validation: Rule => Rule.required(),
+              }),
+            ],
+          }),
+          defineArrayMember({
+            name: 'qux',
+            type: 'object',
+            title: 'Qux',
+            fields: [
+              defineField({
+                name: 'value',
+                type: 'string',
+                validation: Rule => Rule.required(),
+              }),
+            ],
+          }),
         ],
       },
     }),
