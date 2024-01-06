7118b930d9ac0c47c63bcf4b319286e2f3db210c
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-06 03:46:37 -0500

    rearranged folder structure

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,60 @@
+import { Anchor, Badge, Group, Text } from '@mantine/core';
+import Link from 'next/link';
+import PostPagination from '~/components/pagination/post.pagination';
+import { getClient } from '~/lib/sanity/client';
+import classes from './page.module.css';
+
+const client = getClient();
+const ITEMS_PER_PAGE = 10;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { p: string | undefined };
+}) {
+  const pageNumber = Number(searchParams.p) || 1;
+
+  const CATEGORIES_FRAGMENT = /* groq */ `*[_type == "category"]`;
+  const categories = await client.fetch(CATEGORIES_FRAGMENT);
+  const _POSTS_FRAGMENT = /* groq */ `*[_type=="category"]`;
+  const POSTS_FRAGMENT = /* groq */ `{
+    'posts':  ${_POSTS_FRAGMENT}   
+              | order(_updatedAt desc)
+              [($pageIndex * ${ITEMS_PER_PAGE})...($pageIndex + 1) * ${ITEMS_PER_PAGE}],
+    "pagination": {
+      "totalCount": count(${_POSTS_FRAGMENT}._id),
+      "pageNumber": $pageIndex + 1,
+    }
+}`;
+  const { posts, pagination } = await client.fetch(POSTS_FRAGMENT, {
+    pageIndex: pageNumber - 1,
+  });
+
+  return (
+    <>
+      <Text my="sm">
+        Welcome to my blog, collection of random thoughts and occasional
+        tutorials (hopefully)
+      </Text>
+
+      <Group>
+        {categories.map(category => (
+          <Anchor
+            key={category._id}
+            href={`/tag/${category.slug.current}`}
+            component={Link}
+            passHref
+          >
+            <Badge variant="outline" radius="md" className={classes.badge}>
+              {category.title}
+            </Badge>
+          </Anchor>
+        ))}
+      </Group>
+
+      <PostPagination
+        total={Math.ceil(pagination.totalCount / ITEMS_PER_PAGE)}
+        current={pagination.pageNumber}
+      />
+    </>
+  );
+}
