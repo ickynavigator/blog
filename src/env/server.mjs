5069db0869a2a68806c574ca721ad9d4ef26faa0
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-08 00:57:39 -0500

    setup revalidation

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -3,9 +3,11 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    SANITY_API_READ_TOKEN: z.string().default(''),
+    SANITY_API_READ_TOKEN: z.string().optional(),
+    SANITY_REVALIDATE_SECRET: z.string().optional(),
   },
   runtimeEnv: {
     SANITY_API_READ_TOKEN: process.env.SANITY_API_READ_TOKEN,
+    SANITY_REVALIDATE_SECRET: process.env.SANITY_REVALIDATE_SECRET,
   },
 });
