ed63264570f3379536cc78c5a725e52f2894e7b9
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-07 21:51:34 -0500

    disable caching

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -14,6 +14,7 @@ import { getClient } from '~/lib/sanity/client';
 import { SanityValues } from '../../../sanity.config';
 import classes from './page.module.css';
 
+export const dynamic = 'force-dynamic';
 const client = getClient();
 const ITEMS_PER_PAGE = 10;
 export default async function Home({
