5821f3c8f8adf8e4ccf280a168d8a3903e2cbc3f
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-09 21:27:01 -0500

    fix?

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,12 +1,11 @@
 import { createClient } from 'next-sanity';
-import { env } from '~/env/server.mjs';
 import config from '~/lib/sanity/config';
 
 const client = createClient({
   projectId: config.projectId,
   dataset: config.dataset,
   apiVersion: config.apiVersion,
-  useCdn: env.SANITY_REVALIDATE_SECRET ? false : true,
+  useCdn: false,
   perspective: 'published',
 });
 
